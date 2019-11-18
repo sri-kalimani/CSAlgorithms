d70a3465150ad64e850e00d018d5cfd7942581ba
tjcampbel <47730819+tjcampbel@users.noreply.github.com>
2019-11-17 21:42:13 -0500

    just need to comment code runs correctly

--- HW3/graycodesarefun.cpp
+++ HW3/graycodesarefun.cpp
@@ -6,21 +6,21 @@
 #include "vector"
 
 using namespace std;
-
+vector<string> namesInPic(string grayCode);
 string names[] = {"Alice", "Bob", "Chris", "Dylan"};
 
 //Generates recursively the binary reflected Gray code of order n
 //Input: A positive integer n
 //Output: A list of all bit strings of length n composing the Gray code
-vector<int> BRGC(int n)
+vector<string> BRGC(int n)
 {
-    vector<int> listToReturn;
-    vector<int> listReversed;
+    vector<string> listToReturn;
+    vector<string> listReversed;
     int lengthOfint;
     if (n == 1)
     {
-        listToReturn.push_back(0);
-        listToReturn.push_back(1);
+        listToReturn.push_back("0");
+        listToReturn.push_back("1");
     }
     else
     {
@@ -33,23 +33,86 @@ vector<int> BRGC(int n)
 
         for (int i = 0; i < listReversed.size(); i++)
         {
-            listReversed[i] = pow(2, n) + listReversed[i];
+            listToReturn[i] = "0" + listToReturn[i];
+            listReversed[i] = "1" + listReversed[i];
             listToReturn.push_back(listReversed[i]);
         }
     }
 
     return listToReturn;
 }
+int toBinary(string binaryString)
+{
+    int binaryNum = 0;
 
-vector<string> outputOrderOfnames(vector<int> grayCode)
+    for (int i = 0; i < binaryString.length(); i++)
+    {
+        if (binaryString.substr(binaryString.length() - (i + 1), 1) == "1")
+        {
+
+            binaryNum = binaryNum + pow(2, i);
+        }
+    }
+
+    return binaryNum;
+}
+vector<string> outputOrderOfnames(vector<string> grayCode)
+{
+    vector<string> orderOfnames;
+    vector<string> namesInList;
+    int numberShift;
+    int previousNum;
+    int currentNum;
+    string previousString;
+    string currentString;
+    string addToList;
+
+    for (int i = 1; i < grayCode.size(); i++)
+    {
+        previousString = grayCode[i - 1];
+        currentString = grayCode[i];
+        previousNum = toBinary(previousString);
+        currentNum = toBinary(currentString);
+        numberShift = (currentNum ^ previousNum);
+      
+        for (int j = 0; j < 4; j++)
+        {
+            
+            if (numberShift & 1)
+            {
+                namesInList = namesInPic(grayCode[i]);
+                addToList = " ";
+                for(int k =0; k < namesInList.size();k++){
+                    if(namesInList[k] == names[j]){
+                        addToList = names[j] + " in";
+                    }else if(addToList == " "){
+                        addToList = names[j] + " out";
+                    }
+                }
+                orderOfnames.push_back(addToList );
+                numberShift = numberShift >> 1;
+            }
+            else
+            {
+                numberShift = numberShift >> 1;
+            }
+        }
+       
+    }
+    return orderOfnames;
+}
+vector<string> namesInPic(string grayCode)
 {
     vector<string> orderOfnames;
     int numberShift;
+    int previousNum;
+    int currentNum;
+   
     //int sizeOfArray = names.size();
 
-    for (int i = 0; i < grayCode.size(); i++)
-    {
-        numberShift = grayCode[i];
+    
+        numberShift = toBinary(grayCode);
+        // cout << numberShift;
         for (int j = 0; j < 4; j++)
         {
             if (numberShift & 1)
@@ -62,21 +125,33 @@ vector<string> outputOrderOfnames(vector<int> grayCode)
                 numberShift = numberShift >> 1;
             }
         }
-    }
+        
+    
     return orderOfnames;
+}
+
+void createTable(vector<string> grayCode, vector<string> OrderOfnames)
+{
+    vector<string> namesInPhoto;
+    
+    for (int i = 1; i < 16; i++)
+    {
+        cout << i << "   ";
+        cout << grayCode[i] << "  ";
+        namesInPhoto = namesInPic(grayCode[i]);
+        for(int j = 0; j < namesInPhoto.size();j++){
+         cout << namesInPhoto[j] << " ";   
+        }
+        
+        cout << "    " << OrderOfnames[i-1] << "  ";
+        cout << endl;
+    }
+    
 }
 
 int main()
 {
 
-    cout << BRGC(4).size() << endl;
-
-    //vector<string> path = outputOrderOfnames(BRGC(4));
-    vector<int> path = BRGC(4);
-    for (int i = 0; i < path.size(); ++i)
-    {
-        cout << path[i] << endl;
-    }
-
+    createTable(BRGC(4), outputOrderOfnames(BRGC(4)));
     return 1;
 }
