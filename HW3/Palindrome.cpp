4b8b5fd0ea659690a93c2ab51a070234bcfb6e83
sri-kalimani <sriranjani.kalimani@gmail.com>
2019-11-18 15:20:17 -0500

    string cleaning

--- HW3/Palindrome.cpp
+++ HW3/Palindrome.cpp
@@ -10,53 +10,64 @@
 
 using namespace std;
 
-bool PalindromeCheck(int leftside, int rightside, char *&ThePalindromeString) {
-    char *PalindromeArr = ThePalindromeString;
-    bool IsItAPalindrome = false;
+// bool PalindromeCheck(int leftside, int rightside, char *&ThePalindromeString) {
+//     char *PalindromeArr = ThePalindromeString;
+//     bool IsItAPalindrome = false;
+//
+//     if (leftside >= rightside) {
+//         IsItAPalindrome = true;
+//         return IsItAPalindrome;
+//     }
+//
+//     if (PalindromeArr[leftside] != PalindromeArr[rightside]) {
+//         return IsItAPalindrome;
+//     }
+//
+//     return PalindromeCheck(++leftside, --rightside, ThePalindromeString);
+// }
 
-    if (leftside >= rightside) {
-        IsItAPalindrome = true;
-        return IsItAPalindrome;
-    }
+string stringClean(string sentence) {
+    // char StringInput[256];
+    // cout << "Please provide the word or phrase you wish to check" << endl;
+    // cin >> StringInput;
 
-    if (PalindromeArr[leftside] != PalindromeArr[rightside]) {
-        return IsItAPalindrome;
-    }
 
-    return PalindromeCheck(++leftside, --rightside, ThePalindromeString);
-}
+    // int arr_size = strlen(sentence);
+    int arr_size = sentence.length();
+    // char FormattedStringArr[ArraySize];
+    // int FormattedArraySize;
 
-char *StringCleaning() {
-    char StringInput[256];
-    cout << "Please provide the word or phrase you wish to check" << endl;
-    cin >> StringInput;
-    int ArraySize;
-    ArraySize = strlen(StringInput);
-    char FormattedStringArr[ArraySize];
-    int FormattedArraySize;
-
-    for (int i = 0; i < ArraySize; i++) {
-        if (StringInput[i] != '!' || StringInput[i] != '?' || StringInput[i] != '.' ||
-            StringInput[i] != ',') {
-            FormattedStringArr[i] = tolower(StringInput[i]);
+    for (int i=0; i<arr_size; i++){
+      if (!((sentence[i] >= 'a' && sentence[i]<='z') || (sentence[i] >= 'A' && sentence[i]<='Z')))
+        {
+            sentence[i] = '\0';
         }
+        sentence[i] = std::tolower(sentence[i]);
     }
 
-    FormattedArraySize = strlen(FormattedStringArr);
-    char PalindromeString[FormattedArraySize];
+    // for (int i = 0; i < ArraySize; i++) {
+    //     if (StringInput[i] != '!' || StringInput[i] != '?' || StringInput[i] != '.' ||
+    //         StringInput[i] != ',') {
+    //         FormattedStringArr[i] = tolower(StringInput[i]);
+    //     }
+    // }
 
-    for (int i = 0; i < FormattedArraySize; i++) {
-        PalindromeString[i] = FormattedStringArr[i];
-    }
-    return PalindromeString;
+    // FormattedArraySize = strlen(FormattedStringArr);
+    // char PalindromeString[FormattedArraySize];
+    //
+    // for (int i = 0; i < FormattedArraySize; i++) {
+    //     PalindromeString[i] = FormattedStringArr[i];
+    // }
+    return sentence;
 }
 
 
 int main() {
-    char* ThePalindromeString = StringCleaning();
-    int rightside = strlen(ThePalindromeString) - 1;
-    cout << PalindromeCheck(0, rightside, ThePalindromeString) << endl;
-    
+    // char* ThePalindromeString = stringClean();
+    // int rightside = strlen(ThePalindromeString) - 1;
+    // cout << PalindromeCheck(0, rightside, ThePalindromeString) << endl;
 
+    cout<< stringClean("p*nv jJK opk&&&!!!@iop");
+    // cout<<"hello";
     return 0;
 }
