9393e15bc17d35ea2b2a35a9b5a1087185d42e44
tjcampbel <47730819+tjcampbel@users.noreply.github.com>
2019-11-25 17:14:01 -0500

    still doesn't work

--- HW4/mostPreciousPath.cpp
+++ HW4/mostPreciousPath.cpp
@@ -1,216 +1,149 @@
 #include <iostream>
-#include <vector>
-#include<algorithm>
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <math.h>
+#include <array>
 
 using namespace std;
 
-#define ROWS 7
-#define COLS 7
-
-struct Gem_data{
-  int count;
-  int i;
-  int j;
-  Gem_data(int count, int i, int j) : count(count), i(i), j(j) {}
-};
-
-struct Coordinate{
-  int i;
-  int j;
-  Coordinate(int i, int j) : i(i), j(j) {}
-};
-
-struct max_sort
-{
-    inline bool operator() (const Gem_data& struct1, const Gem_data& struct2)
-    {
-        return (struct1.count < struct2.count);
-    }
-};
-
 int gems[][8] = {
-                  {79, 71, 18, 20, 34, 51, 93, 65},
-                  {73, 38, 24, 49, 18, 6, 40, 74},
-                  {31, 5, 63, 10, 32, 40, 14, 13},
-                  {13, 78, 48, 19, 78, 11, 90, 94},
-                  {22, 93, 68, 11, 56, 63, 49, 35},
-                  {85, 52, 27, 5, 94, 91, 82, 62},
-                  {46, 23, 99, 77, 10, 42, 1, 72},
-                  {89, 70, 73, 83, 90, 22, 44, 92}
-                };
+    {79, 71, 18, 20, 34, 51, 93, 65},
+    {73, 38, 24, 49, 18, 6, 40, 74},
+    {31, 5, 63, 10, 32, 40, 14, 13},
+    {13, 78, 48, 19, 78, 11, 90, 94},
+    {22, 93, 68, 11, 56, 63, 49, 35},
+    {85, 52, 27, 5, 94, 91, 82, 62},
+    {46, 23, 99, 77, 10, 42, 1, 72},
+    {89, 70, 73, 83, 90, 22, 44, 92}};
 
-vector<Gem_data> gem_heap;
-int gems_found = 0;
+int dprograming[][8] = {
+    {79, 71, 18, 20, 34, 51, 93, 65},
+    {73, 38, 24, 49, 18, 6, 40, 74},
+    {31, 5, 63, 10, 32, 40, 14, 13},
+    {13, 78, 48, 19, 78, 11, 90, 94},
+    {22, 93, 68, 11, 56, 63, 49, 35},
+    {85, 52, 27, 5, 94, 91, 82, 62},
+    {46, 23, 99, 77, 10, 42, 1, 72},
+    {89, 70, 73, 83, 90, 22, 44, 92}};
 
-
-/* Returns legal neighbors of a given cell */
-vector<vector<int> > neighbors(int i, int j){
-
-    vector<int> index;
-    vector<vector<int> > pair;
-
-    //if cell is left edge: i+1, j; i+1, j+1
-    if (j==0 && i<ROWS){
-      index.push_back(i+1);
-      index.push_back(j);
-      pair.push_back(index);
-      index.clear();
-
-      index.push_back(i+1);
-      index.push_back(j+1);
-      pair.push_back(index);
-      index.clear();
+int path[8][8] = {0};
+int maxNeighbor(int i, int j)
+{
+  if (j == 0)
+  {
+    if (dprograming[i - 1][j] > dprograming[i - 1][j + 1])
+    {
+      return dprograming[i - 1][j];
     }
-    //if cell is right edge: i+1, j; i+1,j-1
-    else if (j==COLS && i<ROWS){
-      index.push_back(i+1);
-      index.push_back(j);
-      pair.push_back(index);
-      index.clear();
-
-      index.push_back(i+1);
-      index.push_back(j-1);
-      pair.push_back(index);
-      index.clear();
-    }
-
-    //not edge cell: i+1, j; i+1, j+1; i+1, j-1
-    else if (i>=0 && i<=ROWS-1 && j>=1 && j<=COLS-1){
-      index.push_back(i+1);
-      index.push_back(j);
-      pair.push_back(index);
-      index.clear();
-
-      index.push_back(i+1);
-      index.push_back(j+1);
-      pair.push_back(index);
-      index.clear();
-
-      index.push_back(i+1);
-      index.push_back(j-1);
-      pair.push_back(index);
-      index.clear();
-    }
-    //out of bounds values or last row
-    else{
-      cout<<"No neighbors \n";
-      return pair;
-    }
-
-    return pair;
-}
-
-Coordinate max_neighbor (vector<vector<int> > neighbor){
-int i=0;
-
-Coordinate max_coor = {neighbor.at(i)[0], neighbor.at(i)[1]};
-
-  while (i < neighbor.size()){
-    if (gems[neighbor.at(i)[0]][neighbor.at(i)[1]] > gems[max_coor.i][max_coor.j]){
-      max_coor.i = neighbor.at(i)[0];
-      max_coor.j = neighbor.at(i)[1];
-    }
-    i++;
+    else
+      return dprograming[i - 1][j + 1];
   }
-return max_coor;
-
-}
-
-Coordinate max_sum_row (vector<vector<int> > sums_row){
-int i=0;
-
-Coordinate max_coor = {sums_row.at(i)[0], sums_row.at(i)[1]};
-
-  while (i < sums_row.size()){
-    if (gems[sums_row.at(i)[0]][sums_row.at(i)[1]] > gems[max_coor.i][max_coor.j]){
-      max_coor.i = sums_row.at(i)[0];
-      max_coor.j = sums_row.at(i)[1];
+  else if (j == 7)
+  {
+    if (dprograming[i - 1][j] > dprograming[i - 1][j - 1])
+    {
+      return dprograming[i - 1][j];
     }
-    i++;
+    else
+      return dprograming[i - 1][j - 1];
   }
-return max_coor;
-
+  else if (dprograming[i - 1][j] > dprograming[i - 1][j + 1] && dprograming[i - 1][j] > dprograming[i - 1][j - 1])
+  {
+    return dprograming[i - 1][j];
+  }
+  else if (dprograming[i - 1][j + 1] > dprograming[i - 1][j - 1])
+  {
+    return dprograming[i - 1][j + 1];
+  }
+  else
+    return dprograming[i - 1][j - 1];
 }
-
-/* For each node, we  */
-int[] preciousPath (){
-
-    // from first row, add max legal value from next row and update sum
-    cout<<"preciousPath"<<endl;
-    int row_sums[8];
-    for (int i=0; i<ROWS; i++){
-      for (int j=0; j<=COLS; j++){
-        row_sums[j] = gems[i][j];
-        cout<<row_sums[j]<< " ";
-      }
-
-      cout<<endl;
-
-      for (int j=0; j<=COLS; j++){
-        vector<vector<int> > walkable_cells = neighbors(i, j);
-        Coordinate max_coor = max_neighbor(walkable_cells);
-        row_sums[j] += gems[max_coor.i][max_coor.j];
-        cout<<row_sums[j]<<" ";
-      }
-
-      cout<<endl;
+void fillIntable()
+{
+  for (int i = 1; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      dprograming[i][j] = dprograming[i][j] + maxNeighbor(i, j);
     }
-
-    return row_sums[8];
-
+  }
 }
+//returns the column of the creator of i , j
+int numberOrgin(int i, int j)
+{
+  int num1 = 0;
+  int num2 = 0;
+  int num3 = 0;
 
-int main(){
+  if (j == 0)
+  {
+    num1 = dprograming[i][j] - dprograming[i - 1][j];
+    num2 = dprograming[i][j] - dprograming[i - 1][j + 1];
+  }
+  else if (j == 7)
+  {
+    num1 = dprograming[i][j] - dprograming[i - 1][j];
+    num3 = dprograming[i][j] - dprograming[i - 1][j - 1];
+  }
+  else
+  {
+    num1 = dprograming[i][j] - dprograming[i - 1][j];
+    num2 = dprograming[i][j] - dprograming[i - 1][j + 1];
+    num3 = dprograming[i][j] - dprograming[i - 1][j - 1];
+  }
 
-  /* Test neighbor function */
-  // int i=0, j=0;
-  // cout<<"Neighbors of 0,1 "<<endl;
-  // vector<vector<int> > index = neighbors(7,7);
-  // for (i=0; i<index.size(); i++){
-  //   for (j=0; j<index[i].size(); j++){
-  //     cout<<index[i][j]<<" ";
-  //   }
-  //   cout<<endl;
-  // }
-  // Coordinate max_coor = max_neighbor(index);
-  // cout<<"Max neigbor is "<<max_coor.i<<", "<<max_coor.j<<endl;
-  // cout<<"Gems at max neighbor is "<<gems[max_coor.i][max_coor.j]<<endl;
-  //
-  // int row, col;
-  // while (index.size()!=0){
-  //     row = index.back()[0];
-  //     col = index.back()[1];
-  //     cout<<gems[row][col]<<endl;
-  //     index.pop_back();
-  // }
-  //
-  // vector <int> all_sums = preciousPath();
-  // int all_sums[8] = preciousPath();
-  // for (int i=0; i<8; i++){
-  //     cout<<all_sums[i]<<endl;
-  // }
+  if (num1 == gems[i - 1][j])
+  {
+    return j;
+  }
+  else if (num2 == gems[i - 1][j + 1])
+  {
+    return (j + 1);
+  }
+  else if (num3 == gems[i - 1][j - 1])
+  {
+    return (j - 1);
+  }
+}
+//updates the path array
+void findPath()
+{
+  int max, column;
+  max = 0;
+  column = 0;
+  for (int i = 0; i < 8; i++)
+  {
+    if (max < dprograming[7][i])
+    {
+      column = i;
+    }
+  }
+  path[7][column] = 1;
+  printf("The maximum gems: %d", max);
+  for (int i = 7; i > 0; i--)
+  {
+    column = numberOrgin(i, column);
+    path[i][column] = 1;
+  }
+}
+int main()
+{
+  fillIntable();
+  findPath();
 
-preciousPath();
-  /* Test max_heap */
-/*
-  Gem_data gm1 = {gems[0][0], 0, 0};
-  Gem_data gm2 = {gems[0][1], 0, 1};
-  Gem_data gm3 = {gems[7][7], 7, 7};
-  Gem_data gm4 = {gems[1][1], 1, 1};
-
-  vector<Gem_data> v1;
-  v1.push_back(gm1);
-  v1.push_back(gm2);
-  v1.push_back(gm3);
-  v1.push_back(gm4);
-  v1.push_back(Gem_data(gems[2][2], 2, 2));
-
-  make_heap(v1.begin(), v1.end(), max_sort());
-  cout << v1.front().i << ","<< v1.front().j<<endl;
-  v1.pop_back();
-  cout << v1.back().i << ","<< v1.back().j<<endl;
-*/
-
-  return 0;
+  /*for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            printf("%d  ", dprograming[i][j]);
+        }
+        cout << endl;
+    }*/
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      printf("%d  ", path[i][j]);
+    }
+    cout << endl;
+  }
 }
--- HW4/mostPreciousPath2.cpp
+++ HW4/mostPreciousPath2.cpp
@@ -25,6 +25,7 @@ int dprograming[][8] = {
     {46, 23, 99, 77, 10, 42, 1, 72},
     {89, 70, 73, 83, 90, 22, 44, 92}};
 
+int path[8][8] = {0};
 int maxNeighbor(int i, int j)
 {
     if (j == 0)
@@ -66,16 +67,82 @@ void fillIntable()
         }
     }
 }
+//returns the column of the creator of i , j
+int numberOrgin(int i, int j)
+{
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
 
+    if (j == 0)
+    {
+        num1 = dprograming[i][j] - dprograming[i - 1][j];
+        num2 = dprograming[i][j] - dprograming[i - 1][j + 1];
+    }
+    else if (j == 7)
+    {
+        num1 = dprograming[i][j] - dprograming[i - 1][j];
+        num3 = dprograming[i][j] - dprograming[i - 1][j - 1];
+    }
+    else
+    {
+        num1 = dprograming[i][j] - dprograming[i - 1][j];
+        num2 = dprograming[i][j] - dprograming[i - 1][j + 1];
+        num3 = dprograming[i][j] - dprograming[i - 1][j - 1];
+    }
+
+    if (num1 == gems[i - 1][j])
+    {
+        return j;
+    }
+    else if (num2 == gems[i - 1][j + 1])
+    {
+        return (j + 1);
+    }
+    else if (num3 == gems[i - 1][j - 1])
+    {
+        return (j - 1);
+    }
+}
+//updates the path array
+void findPath()
+{
+    int max, column;
+    max = 0;
+    column = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        if (max < dprograming[7][i])
+        {
+            column = i;
+        }
+    }
+    path[7][column] = 1;
+    printf("The maximum gems: %d", max);
+    for (int i = 7; i > 0; i--)
+    {
+        column = numberOrgin(i, column);
+        path[i][column] = 1;
+    }
+}
 int main()
 {
     fillIntable();
+    findPath();
 
-    for (int i = 0; i < 8; i++)
+    /*for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
             printf("%d  ", dprograming[i][j]);
+        }
+        cout << endl;
+    }*/
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            printf("%d  ", path[i][j]);
         }
         cout << endl;
     }
