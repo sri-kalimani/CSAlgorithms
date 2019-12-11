02adbe926bffdc27c21229ed69e883b27c91daa2
tjcampbel <47730819+tjcampbel@users.noreply.github.com>
2019-12-11 15:49:45 -0500

    start of part 6

---
+++ HW6/nqueens.cpp
@@ -0,0 +1,135 @@
+
+
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <array>
+
+using namespace std;
+
+void printBoard(int *queenRank, int boardSize)
+{
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            queenRank[i] == j ? cout << "1 " : cout << "0 ";
+        }
+        cout << endl;
+    }
+}
+
+void successor(int *queenRank, int boardSize)
+{
+    for (int i = 0; i < boardSize; i++)
+    {
+        if (queenRank[i] == -1)
+        {
+            for (int j = 0; j < boardSize; j++)
+            {
+                if (find(queenRank, queenRank + i, j) == (queenRank + i))
+                {
+                    queenRank[i] = j;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+}
+
+bool isLegalPosition(int *queenRank, int boardSize)
+{
+    int board[boardSize][boardSize]; // create the board
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; i++)
+        {
+            board[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < boardSize; i++)
+    {
+        if (queenRank[i] != -1)
+        {                               // -1 means leave that row blank
+            board[i][queenRank[i]] = 1; // put a 1 in each row at the correct rank
+        }
+    }
+
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            if (board[i][j] == 1)
+            { // check every queen
+                int k, l;
+
+                for (k = (i + 1), l = (j + 1); k < boardSize && l < boardSize; k++, l++)
+                {
+                    if (board[k][l] == 1)
+                    {
+                        return false;
+                    }
+                }
+                for (k = (i + 1); k < boardSize; k++)
+                {
+                    if (board[k][j] == 1)
+                    {
+                        return false;
+                    }
+                }
+                for (l = (j + 1); l < boardSize; l++)
+                {
+                    if (board[i][l] == 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+void nextLegalPosition(int *queenRank, int boardSize)
+{
+}
+
+int main()
+{
+    int boardSize;
+    int queenPositions;
+    cout << "How big is the board?" << endl;
+    cin >> boardSize;
+
+    int* queenRank = new int[boardSize];
+
+    cout << "Enter the positions for the queens from 1 to board size and enter -1 if you would like it to be a partial board" << endl;
+    int breakLoc = 0;
+    for(int i =0; i < boardSize;i++){
+        cin >> queenPositions;
+        if(queenPositions == -1){
+            breakLoc = i;
+            break;
+        }else if(queenPositions < 1 || queenPositions > boardSize){
+            cout << "Error not valid input please try again" << endl;
+            i = i-1;
+        }
+        else {
+            queenRank[i] =queenPositions-1;
+        }
+    }
+    if(breakLoc != 0){
+        for(int i =breakLoc; i < boardSize; i++){
+            queenRank[i]= -1;
+        }
+    }
+        for(int i =0;i < boardSize;i++){
+            cout << queenRank[i] << endl;
+        }
+
+        return 1;
+}
